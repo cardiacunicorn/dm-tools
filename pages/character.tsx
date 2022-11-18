a7803909f056b8df164c96646d0bbe2525a0ef90
alex <alexm@schwartzmedia.com.au>
2022-11-18 15:25:20 +1100

    loading monsters

--- pages/character.tsx
+++ pages/character.tsx
@@ -1,9 +1,51 @@
+import { useState, useEffect } from 'react'
+import useSWR from 'swr'
+
+const characterURL = 'https://character-service.dndbeyond.com/character/v5/character/75936428'
+const [characterData, setCharacterData] = useState([
+  {}
+])
+// const res = await fetch(characterURL)
+// const articles = await res.json()
+
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+// useEffect(() => {
+//   fetch(characterURL)
+//       .then(response => response.json())
+//       .then(data => setCharacterData(data.data));
+
+// // empty dependency array means this effect will only run once (like componentDidMount in classes)
+// }, []);
 
 
-const character = () => {
+
+export default function Character({character}) {
+
+  // const { data, error } = useSWR(
+  //   characterURL,
+  //   fetcher
+  // );
+  
+  // if (error) return "An error has occurred.";
+  // if (!data) return "Loading...";
+
   return (
-    <div>character</div>
+    <div>
+      <code>{characterURL}</code>
+      <code>{character}</code>
+    </div>
   )
 }
 
-export default character
+// Adding data functions
+export const getStaticProps = async () => {
+  const res = await fetch(characterURL)
+  const character = await res.json()
+
+  return {
+      props: {
+          character
+      }
+  }
+}
---
+++ pages/monster.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import styles from '../styles/Monsters.module.scss'
+
+export default function Monster({ }) {
+    const monstersURL = "https://monster-service.dndbeyond.com/v1/monster"
+    const monsterURL = "https://monster-service.dndbeyond.com/v1/monster/16762"
+    const [monstersData, setMonstersData] = useState()
+
+    let displayData
+    function pullJson() {
+        fetch(monstersURL)
+            .then(res => res.json())
+            .then(json => {
+                console.log(json.data)
+                displayData = json.data.map(function(monster) {
+                    return(
+                        <table key={monster.id}>
+                            <tr>
+                                <th className={styles.table_head}>
+                                    ID
+                                </th>
+                                <th className={styles.table_head}>
+                                    Name
+                                </th>
+                            </tr>
+                            <tr>
+                                <td className={styles.table_data}>
+                                    {monster.id}
+                                </td>
+                                <td className={styles.table_data}>
+                                    {monster.name}
+                                </td>
+                            </tr>
+                        </table>
+                    )
+                })
+                setMonstersData(displayData)
+            })
+    }
+
+    useEffect(() => {
+        pullJson()
+    },[])
+
+    return (
+        <div>
+            <h1>Monsters</h1>
+            {monstersData}
+        </div>
+    )
+}

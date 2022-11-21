41605304e0d9a573599992c9d6cc284d98ae5c0e
alex <alexm@schwartzmedia.com.au>
2022-11-22 10:23:23 +1100

    additional fantasy fonts

--- pages/account.tsx
+++ pages/account.tsx
@@ -4,7 +4,21 @@ import fonts from '../styles/Fonts.module.scss'
 export default function Account() {
     return (
         <div>
-            Norse Runes:<br /><span className={fonts.norse}>Account Page</span>
+            <div>
+                Norse Runes:<br /><span className={fonts.norse}>Account Page</span>
+            </div>
+            <div>
+                Dwarvish/Giant/Gnomish/Goblin/Primordial:<br /><span className={fonts.dwarvish}>Account Page</span>
+            </div>
+            <div>
+                Draconic/Orc:<br /><span className={fonts.draconic}>Account Page</span>
+            </div>
+            <div>
+                Abyssal/Infernal:<br /><span className={fonts.infernal}>Account Page</span>
+            </div>
+            <div>
+                Elvish/Sylvan/Undercommon:<br /><span className={fonts.elvish}>Account Page</span>
+            </div>
         </div>
     )
 }

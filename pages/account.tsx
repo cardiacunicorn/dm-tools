78dfa374bb1286954ac23b7413e27993ba7aa3a2
alex <alexm@schwartzmedia.com.au>
2022-11-22 09:47:37 +1100

    implemented norse font

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,9 +1,10 @@
 import styles from '../styles/Account.module.scss'
+import fonts from '../styles/Fonts.module.scss'
 
 export default function Account() {
     return (
         <div>
-            Account Page
+            Norse Runes:<br /><span className={fonts.norse}>Account Page</span>
         </div>
     )
 }

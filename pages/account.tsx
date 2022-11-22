9a082b00607804a6c73f86083a18969774b0eed8
alex <alexm@schwartzmedia.com.au>
2022-11-22 11:36:16 +1100

    dnd fonts

--- pages/account.tsx
+++ pages/account.tsx
@@ -4,23 +4,29 @@ import fonts from '../styles/Fonts.module.scss'
 export default function Account() {
     return (
         <div>
-            <div>
-                Norse/Giant Runes:<br /><span className={fonts.norse}>Account Page</span>
+            <div className={styles.font_box}>
+                <div className={styles.font_descriptor}>Norse/Giant Runes:</div>
+                <div className={fonts.norse}>Account Page</div>
             </div>
-            <div>
-                Dwarvish/Gnomish/Goblin/Primordial:<br /><span className={fonts.dwarvish}>Account Page</span>
+            <div className={styles.font_box}>
+                <div className={styles.font_descriptor}>Dwarvish/Gnomish/Goblin/Primordial:</div>
+                <div className={fonts.dwarvish}>Account Page</div>
             </div>
-            <div>
-                Draconic/Orc:<br /><span className={fonts.draconic}>Account Page</span>
+            <div className={styles.font_box}>
+                <div className={styles.font_descriptor}>Draconic/Orc:</div>
+                <div className={fonts.draconic}>Account Page</div>
             </div>
-            <div>
-                Abyssal/Infernal:<br /><span className={fonts.infernal}>Account Page</span>
+            <div className={styles.font_box}>
+                <div className={styles.font_descriptor}>Abyssal/Infernal:</div>
+                <div className={fonts.infernal}>Account Page</div>
             </div>
-            <div>
-                Elvish/Sylvan/Undercommon:<br /><span className={fonts.elvish}>Account Page</span>
+            <div className={styles.font_box}>
+                <div className={styles.font_descriptor}>Elvish/Sylvan/Undercommon:</div>
+                <div className={fonts.elvish}>Account Page</div>
             </div>
-            <div>
-                Arcane/Celestial:<br /><span className={fonts.arcane}>Account Page</span>
+            <div className={styles.font_box}>
+                <div className={styles.font_descriptor}>Arcane/Celestial:</div>
+                <div className={fonts.arcane}>Account Page</div>
             </div>
         </div>
     )

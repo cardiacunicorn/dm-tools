c0ee9766cd4e1e3ce2ce5f32683ffac5fc98749d
alex <alexm@schwartzmedia.com.au>
2022-11-22 11:42:28 +1100

    font colours

--- pages/account.tsx
+++ pages/account.tsx
@@ -7,26 +7,35 @@ export default function Account() {
             <div className={styles.font_box}>
                 <div className={styles.font_descriptor}>Norse/Giant Runes:</div>
                 <div className={fonts.norse}>Account Page</div>
+                <div className={fonts.giant}>Account Page</div>
             </div>
             <div className={styles.font_box}>
                 <div className={styles.font_descriptor}>Dwarvish/Gnomish/Goblin/Primordial:</div>
                 <div className={fonts.dwarvish}>Account Page</div>
+                <div className={fonts.gnomish}>Account Page</div>
+                <div className={fonts.goblin}>Account Page</div>
+                <div className={fonts.primordial}>Account Page</div>
             </div>
             <div className={styles.font_box}>
                 <div className={styles.font_descriptor}>Draconic/Orc:</div>
                 <div className={fonts.draconic}>Account Page</div>
+                <div className={fonts.orc}>Account Page</div>
             </div>
             <div className={styles.font_box}>
                 <div className={styles.font_descriptor}>Abyssal/Infernal:</div>
+                <div className={fonts.abyssal}>Account Page</div>
                 <div className={fonts.infernal}>Account Page</div>
             </div>
             <div className={styles.font_box}>
                 <div className={styles.font_descriptor}>Elvish/Sylvan/Undercommon:</div>
                 <div className={fonts.elvish}>Account Page</div>
+                <div className={fonts.sylvan}>Account Page</div>
+                <div className={fonts.undercommon}>Account Page</div>
             </div>
             <div className={styles.font_box}>
                 <div className={styles.font_descriptor}>Arcane/Celestial:</div>
                 <div className={fonts.arcane}>Account Page</div>
+                <div className={fonts.celestial}>Account Page</div>
             </div>
         </div>
     )

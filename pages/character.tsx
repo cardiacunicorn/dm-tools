06f2f4a9070f43abac5be4fb5a4efe5247ae43f7
alex <alexm@schwartzmedia.com.au>
2022-11-25 15:41:19 +1100

    WIP

--- pages/character.tsx
+++ pages/character.tsx
@@ -1,46 +1,33 @@
 import { useState, useEffect } from 'react'
 import styles from '../styles/Character.module.scss'
 
-// const characterURL = 'https://character-service.dndbeyond.com/character/v5/character/75936428'
-const characterURL = 'https://api.github.com/users/deekshasharma'
+// const url = 'https://character-service.dndbeyond.com/character/v5/character/75936428'
+const url = 'https://api.github.com/users/deekshasharma'
 
 export default function Character() {
 
-  const [characterData, setCharacterData] = useState([])
+  const [characterData, setCharacterData] = useState({})
 
-  let displayData
-
-  const pullJson = async () => {
-
-    const res = await fetch(characterURL);
-    const jsonData = await res.json();
-
-    displayData = jsonData.data.map(function(character) {
-        return(
-            <div>
-              {character.name}
-            </div>
-        )
-    })
-    setCharacterData(displayData);
-
-    // fetch(characterURL)
-    //   .then(res => res.json())
-    //   .then(json => {
-    //       console.log(json.data)
-    //       displayData = json.data
-    //       setCharacterData(displayData)
-    //   })
+  const fetchData = () => {
+    fetch(url)
+      .then(res => {
+          return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        setCharacterData(data)
+      })
   }
 
   useEffect(() => {
-      pullJson();
+      fetchData()
   },[]);
 
   return (
     <div>
-      <code>{characterURL}</code>
-      <code>{characterData}</code>
+      <code>{url}</code>
+      {/* Must render exact line items, not an object */}
+      <code>{characterData.name}</code>
     </div>
   )
 }

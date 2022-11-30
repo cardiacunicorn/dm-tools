0f2abfb17464d471bd1272662011a329767c68a1
alex <alexm@schwartzmedia.com.au>
2022-11-30 16:50:19 +1100

    Added notion SDK & ids

--- components/articles/ArticleList.tsx
+++ components/articles/ArticleList.tsx
@@ -4,9 +4,9 @@ import styles from '../../styles/Article.module.scss'
 const ArticleList = ({ articles }) => {
   return (
     <div className={styles.grid}>
-        {articles.map((article) => (
-            <ArticleItem article={article} />
-        ))}
+      {articles.map((article) => (
+        <ArticleItem article={article} />
+      ))}
     </div>
   )
 }

10a78d8237ae43a887eed3849b9def82a3f26226
alex <alexanderwmills@gmail.com>
2022-12-14 10:23:01 +1100

    renaming assets

--- pages/sessions.tsx
+++ pages/sessions.tsx
@@ -1,12 +1,19 @@
 import styles from '../styles/Sessions.module.scss'
+import downChevron from '../assets/icons/directional/chevron-down-thick.png'
 
 export default function Sessions() {
+
+  console.log(downChevron)
+
   return (
     <div className={styles.sessions_page}>
         <div className={styles.accordion}>
           {data.map((item, i) => (
             <div className={styles.accordion_item}>
-              <h2 className={styles.item_title}>{item.title}</h2>
+              <div className={styles.item_title}>
+                <h2>{item.title}</h2>
+                <img src={downChevron.src} alt="Open/Close" />
+              </div>
               <div className={styles.item_body}>{item.body}</div>
             </div>
           ))}

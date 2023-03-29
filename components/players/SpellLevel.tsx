c5c70d79bec2c984ab63616b62f7245367d7386d
alex <alexanderwmills@gmail.com>
2023-03-29 17:19:11 +1100

    spell levels

--- components/players/SpellLevel.tsx
+++ components/players/SpellLevel.tsx
@@ -2,10 +2,14 @@ import styles from '../../styles/Players.module.scss'
 
 const SpellLevel = ({ spellLevel }) => {
 
+  // Create an Array from Uses number
+  var total = Array.from(Array(spellLevel.total).keys());
+  var used = Array.from(Array(spellLevel.used).keys());
+
   return (
       <div className={styles.spell_level}>
         <div className={styles.attack_name}>
-            {spellLevel.level}
+            Level {spellLevel.level}: {spellLevel.used}/{spellLevel.total}
         </div>
       </div>
   )
